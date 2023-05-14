8e8be1ea9ead04361a1e7dbb4fbf4302d8a2c294
Lahiru Madushanka <lahirumadushandl@gmail.com>
2023-05-14 16:15:14 +0530

    Initial Commit

--- gym_exercise/src/components/SearchExercises.jsx
+++ gym_exercise/src/components/SearchExercises.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import { Box, Button, Stack, TextField,Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercise = () => {
+const SearchExercise = ({setExercises,bodyPart,setBodyPart}) => {
   const [search, setsearch] = useState('');
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
+
 
   useEffect(() => {
       const fetchExercisesData = async () =>{
@@ -73,7 +74,9 @@ const SearchExercise = () => {
           </Button>
       </Box>
       <Box sx={{position: 'relative', width:'100%',p: '20px'}}>
-        <HorizontalScrollBar data={bodyParts} />
+        <HorizontalScrollbar data={bodyParts}
+          bodyPart={bodyPart} setBodyPart={setBodyPart} 
+        />
       </Box>
 
     </Stack>
